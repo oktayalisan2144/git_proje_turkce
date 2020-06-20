15d11521d31aefcaa97ec5739d1313a1e02e202a
oktayalisan2144 <67158796+oktayalisan2144@users.noreply.github.com>
2020-06-20 22:44:31 +0300

    Update FirstClass.java
    artik cozum ariyoruz

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -9,7 +9,7 @@ public class FirstClass {
 
 		
 
-		System.out.println("conflict yapmak istiyoruz");
+		System.out.println("we want to make the second conflict");
 
 		System.out.println("first commit from remote repository");
 	}
